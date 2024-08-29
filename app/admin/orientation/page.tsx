e158bd7d783000bd05bc0c7bdfed33c6ab55e709
Jhay3765 <jayvolney77@gmail.com>
2024-08-28 23:19:32 -0400

    control

--- app/admin/orientation/page.tsx
+++ app/admin/orientation/page.tsx
@@ -1,17 +1,22 @@
+// import React from "react";
+// import { createClient } from "@/utils/supabase/server";
+// import { redirect } from "next/navigation";
+
+// import { getUnverifiedUsers } from "@/utils/supabase/supabaseQueries";
+// import VerifyUsersPage from "./UserDisplay";
+// export default async function page() {
+//   const drivers = await getUnverifiedUsers();
+//   console.log(drivers);
+
+//   return (
+//     <div>
+//       {" "}
+//       <VerifyUsersPage />{" "}
+//     </div>
+//   );
+// }
 import React from "react";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
-import { getUnverifiedUsers } from "@/utils/supabase/supabaseQueries";
-import VerifyUsersPage from "./UserDisplay";
-export default async function page() {
-  const drivers = await getUnverifiedUsers();
-  console.log(drivers);
-
-  return (
-    <div>
-      {" "}
-      <VerifyUsersPage />{" "}
-    </div>
-  );
+export default function page() {
+  return <div>page</div>;
 }
