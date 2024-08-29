214fe04b03409955f6acb23f784a99008d773166
Jhay3765 <jayvolney77@gmail.com>
2024-08-28 23:15:00 -0400

    try

--- app/admin/drivers/page.tsx
+++ app/admin/drivers/page.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import DriversPage from "./DriverCard";
-import { getDrivers } from "@/utils/supabase/supabaseQueries";
-export default async function page() {
-  const drivers = await getDrivers();
-  console.log(drivers);
+// import React from "react";
+// import { createClient } from "@/utils/supabase/server";
+// import { redirect } from "next/navigation";
+// import DriversPage from "./DriverCard";
+// import { getDrivers } from "@/utils/supabase/supabaseQueries";
+// export default async function page() {
+//   const drivers = await getDrivers();
+//   console.log(drivers);
 
-  return <div>{drivers && <DriversPage drivers={drivers} />}</div>;
+//   return <div>{drivers && <DriversPage drivers={drivers} />}</div>;
+// }
+
+import React from "react";
+
+export default function page() {
+  return <div>page</div>;
 }
