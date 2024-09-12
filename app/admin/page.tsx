bd934abb7ea091fea0640c03379aba723ecb552c
Jhay3765 <jayvolney77@gmail.com>
2024-09-12 14:03:14 -0400

    cahnges

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import AddDriverForm from "../drivers/driver-form/DriverForm";
-import DriversPage from "./drivers/page";
+
 export default async function page() {
   return (
     <div className="p-8">
