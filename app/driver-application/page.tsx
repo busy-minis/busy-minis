ebf61a43ada324e26461261e169838e9ae8fa16b
Jhay3765 <jayvolney77@gmail.com>
2024-09-12 14:08:11 -0400

    changes

--- app/driver-application/page.tsx
+++ app/driver-application/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AddDriverForm from "./DriverForm";
-
+import { Suspense } from "react";
 export default function page() {
   return (
     <div className="h-screen ">
-      <AddDriverForm />
+      <Suspense fallback={null}>
+        <AddDriverForm />
+      </Suspense>
     </div>
   );
 }
